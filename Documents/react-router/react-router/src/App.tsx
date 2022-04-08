e4b71f2944f4959c45f56c42a582627821ec5972
shubham <shubham.cc@gmail.com>
2022-04-08 06:19:25 +0530

    sentry used

--- Documents/react-router/react-router/src/App.tsx
+++ Documents/react-router/react-router/src/App.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import * as Sentry from "@sentry/react";
+import logo from "./logo.svg";
+import "./App.css";
+import Contact from "./components/Contact";
+import About from "./components/About";
 
 function App() {
+  const navigate = useNavigate();
+  const aboutCalled = () => {
+    
+    navigate("about");
+  };
+  const contactCalled = () => {
+    navigate("contact");
+  };
   return (
     <div className="App">
-     <h1>React Router</h1>
+      <button onClick={aboutCalled}>Hellooo about</button>
+      <button onClick={contactCalled}>Hellooo Contact</button>
+      
+      <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />}></Route>
+      </Routes>
     </div>
   );
 }
 
-export default App;
+export default Sentry.withProfiler(App)
--- Documents/react-router/react-router/src/index.tsx
+++ Documents/react-router/react-router/src/index.tsx
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
+
+
+Sentry.init({
+  dsn: "https://486a71138c25404caaec72ff02e70bf6@o1193935.ingest.sentry.io/6316290",
+  integrations: [new BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
+  <BrowserRouter>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
